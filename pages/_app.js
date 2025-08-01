7936a8baac84bc7c35d206f42b1e0a7c1c3efd3a
SarKLeRoi <khamissari97@gmail.com>
2025-08-02 01:04:14 +0300

    Fix Scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,44 @@
 // pages/_app.js
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/global.css"; // You can import global styles here
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Scroll to top when route changes
+    const handleRouteChange = () => {
+      // Force scroll to top with multiple methods
+      setTimeout(() => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "instant",
+        });
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        document.documentElement.scrollLeft = 0;
+        document.body.scrollLeft = 0;
+      }, 0);
+    };
+
+    // Listen for route changes
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // Cleanup listeners on unmount
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
+  // Also scroll to top on initial load
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
