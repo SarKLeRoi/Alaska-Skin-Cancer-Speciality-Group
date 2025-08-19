7543eb90e0bf052e7e935c2199012ec00bda6560
SarKLeRoi <khamissari97@gmail.com>
2025-08-19 23:39:21 +0300

    Google Search Console Fixes

--- next.config.js
+++ next.config.js
@@ -3,28 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: "/Alaska",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "index, follow",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
